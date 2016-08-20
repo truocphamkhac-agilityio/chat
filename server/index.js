cc77de7470ed29ce24f9c8e08d52711e2d58842d
Truoc Pham <truoc.phamkhac@asnet.com.vn>
2016-08-20 15:23:05 +0700

    Using socket.io to implement chating

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 'use strict';
 
-const express = require('express');
-const path = require('path');
 const PORT = 8020;
-
+const path = require('path');
+const express = require('express');
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 app.use(express.static(path.join(__dirname, '../public')));
 
@@ -12,6 +13,17 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-app.listen(PORT, () => {
+io.on('connection', socket => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', msg => {
+        console.log('user chat message');
+        io.emit('chat message', msg);
+    });
+});
+
+server.listen(PORT, () => {
     console.log('Server already listen on', PORT);
 });
